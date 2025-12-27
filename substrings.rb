7e04404121035377ffb8193d2da05190e55e6d20
Vinicius Chaves <vinicius@Xubuntu.myguest.virtualbox.org>
2025-12-27 10:01:25 -0300

    Add substrings method. It checks if a string is contained in a dicionary and returns all the matches

--- substrings.rb
+++ substrings.rb
@@ -1,5 +1,22 @@
+# require 'pry-byebug'; binding.pry
+def substrings (word, dictionary)
+  match_words = Hash.new(0)
+
+  dictionary.each do |type|
+    if includes_word?(type, word)
+      match_words[type] += 1
+    end
+  end
+
+  match_words
+end
+
 def includes_word? (contained_word, container_word)
+  contained_word = contained_word.split('')
+  container_word = container_word.split('')
   contained_word.all? { |character| container_word.include?(character) }
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+puts substrings('below', dictionary)
