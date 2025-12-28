4e0fadfe6f92515b123840894d6e656d6f82f438
Vinicius Chaves <vinicius@Xubuntu.myguest.virtualbox.org>
2025-12-28 08:53:29 -0300

    Fix substrings logic: Now all words are counted correctly

--- substrings.rb
+++ substrings.rb
@@ -1,22 +1,23 @@
 # require 'pry-byebug'; binding.pry
-def substrings (word, dictionary)
+def substrings (string, dictionary)
   match_words = Hash.new(0)
-
+  words = string.split(' ')
   dictionary.each do |type|
-    if includes_word?(type, word)
-      match_words[type] += 1
+    words.each do |word|
+      if includes_word?(type, word)
+        match_words[type] += 1
+      end
     end
   end
-
   match_words
 end
 
 def includes_word? (contained_word, container_word)
-  contained_word = contained_word.split('')
-  container_word = container_word.split('')
+  contained_word = contained_word.downcase.split('')
+  container_word = container_word.downcase.split('')
   contained_word.all? { |character| container_word.include?(character) }
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-puts substrings('below', dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
